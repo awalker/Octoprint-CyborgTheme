72f68fc87fe47d04b0b4e59cd0fd004d79678f0b
Adam Walker <awalker@virtualmindset.com>
2017-04-10 10:30:47 -0400

    Darkly

--- octoprint_cyborgtheme/__init__.py
+++ octoprint_cyborgtheme/__init__.py
@@ -7,8 +7,8 @@ class CyborgThemePlugin(octoprint.plugin.AssetPlugin):
 
     def get_assets(self):
         return dict(
-            css=["css/cyborg.css", "css/overrides-min.css", "css/bootstrap-modal.css"]
+            css=["css/darkly.css", "css/overrides-min.css", "css/bootstrap-modal.css"]
         )
 
-__plugin_name__ = "Cyborg Theme"
+__plugin_name__ = "Darkly Theme"
 __plugin_implementation__ = CyborgThemePlugin()
